68e504fa1a3099f8713c25d077fc2ee792298070
Gerardo Mendoza Aguilar <germenag@gmail.com>
2012-03-18 14:41:03 -0600

    Added entity class

---
+++ src/main/java/com/poliformas/warehouse/entity/Marks.java
@@ -0,0 +1,37 @@
+package com.poliformas.warehouse.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "WAREHOUSE_MARKS")
+public class Marks {
+	
+	@Id
+	@Column(name="ID_Mark")
+	@GeneratedValue
+	private int id_mark;
+	
+	@Column(name="Name")
+	private String name;
+
+	public int getId_mark() {
+		return id_mark;
+	}
+
+	public void setId_mark(int id_mark) {
+		this.id_mark = id_mark;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
