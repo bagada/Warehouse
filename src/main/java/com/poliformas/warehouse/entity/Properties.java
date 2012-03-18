86599a7405a880033e4b1949379d85acd3998bd2
Teresa Duarte <tereds15@gmail.com>
2012-03-18 15:57:38 -0600

    Added Entity Class

---
+++ src/main/java/com/poliformas/warehouse/entity/Properties.java
@@ -0,0 +1,82 @@
+package com.poliformas.warehouse.entity;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "WHAREHOUSE_WHAREHOUSE")
+
+public class Properties { 
+	@Id
+	@Column(name ="ID_PROPERTIES")
+	@GeneratedValue
+	private  int idproperties;
+
+	@Column(name ="ID_EQUIPMENT")
+	private  int idequipment;
+	
+	@Column(name ="CODE")
+	private   String code;
+	
+	@Column(name ="DATE_BUY")
+	private  Date datebuy;
+	
+	@Column(name ="DATE_WARRANTY")
+	private  Date datewarranty;
+	
+	@Column(name ="SERIE")
+	private  int serie;
+
+	public int getIdproperties() {
+		return idproperties;
+	}
+
+	public void setIdproperties(int idproperties) {
+		this.idproperties = idproperties;
+	}
+
+	public int getIdequipment() {
+		return idequipment;
+	}
+
+	public void setIdequipment(int idequipment) {
+		this.idequipment = idequipment;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public Date getDatebuy() {
+		return datebuy;
+	}
+
+	public void setDatebuy(Date datebuy) {
+		this.datebuy = datebuy;
+	}
+
+	public Date getDatewarranty() {
+		return datewarranty;
+	}
+
+	public void setDatewarranty(Date datewarranty) {
+		this.datewarranty = datewarranty;
+	}
+
+	public int getSerie() {
+		return serie;
+	}
+
+	public void setSerie(int serie) {
+		this.serie = serie;
+	}
+}
---
+++ src/main/java/com/poliformas/warehouse/entity/Warehouse.java
@@ -0,0 +1,71 @@
+package com.poliformas.warehouse.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "WHAREHOUSE_WHAREHOUSE")
+
+public class Warehouse {
+	@Id
+	@Column(name ="ID_WAREHOUSE")
+	@GeneratedValue
+	private  int idwarehouse;
+	
+	@Column(name ="ID_COMPONENT")
+	private  int idcomponent;
+	
+	@Column(name ="ID_EQUIPMENT")
+	private  int idequipment;
+	
+	@Column(name ="ID_ADDRESS")
+	private  int idaddress;
+	
+	@Column(name ="STATUS")
+	private  String status;
+
+	public int getIdwarehouse() {
+		return idwarehouse;
+	}
+
+	public void setIdwarehouse(int idwarehouse) {
+		this.idwarehouse = idwarehouse;
+	}
+
+	public int getIdcomponent() {
+		return idcomponent;
+	}
+
+	public void setIdcomponent(int idcomponent) {
+		this.idcomponent = idcomponent;
+	}
+
+	public int getIdequipment() {
+		return idequipment;
+	}
+
+	public void setIdequipment(int idequipment) {
+		this.idequipment = idequipment;
+	}
+
+	public int getIdaddress() {
+		return idaddress;
+	}
+
+	public void setIdaddress(int idaddress) {
+		this.idaddress = idaddress;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+}
